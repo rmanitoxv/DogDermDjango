4c8dd857e17b80e6d6484cbbec7e77fabf4f699c
Russel Manito <manito.russel@gmail.com>
2022-12-12 04:42:55 +0800

    add vue to cors

--- dogderm/settings.py
+++ dogderm/settings.py
@@ -29,7 +29,8 @@ ALLOWED_HOSTS = []
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:5173"
 ]
 
 REST_FRAMEWORK = {
